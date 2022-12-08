56139da692e47d3aa4685c31d4c2a0ec62389a11
Marlon (esolitos) Saglia <marlon@nymedia.no>
2022-12-08 23:26:45 +0100

    chore: cleanup and comments

--- swa/spotify_weekly.py
+++ swa/spotify_weekly.py
@@ -1,7 +1,6 @@
 import logging
 
 from typing import Dict, List, Optional
-from spotipy.util import prompt_for_user_token
 from spotipy import Spotify
 
 class SwaError(RuntimeError):
@@ -47,12 +46,18 @@ class SwaRunner:
         self._discover_weekly_id = discover_weekly_id if discover_weekly_id else None
 
     def run(self):
+        """
+        Main runtime.
+        """
         album_playlist = self.prepare_weekly_album_playlist()
         album_ids = self.get_weekly_albums_ids()
         tracks = self.get_all_albums_tracks(album_ids)
         self.add_tracks_to_playlist(album_playlist['id'], tracks)
 
     def get_user(self) -> Dict:
+        """
+        Will return a user dictionary.
+        """
         if self._user is None:
             self._user = self._spy_client.current_user()
             logging.debug('Fetched user data:')
@@ -68,6 +73,9 @@ class SwaRunner:
         return self.get_user()['id']
 
     def get_user_playlists(self, sort_by_author: bool = False) -> List[Dict]:
+        """
+        List all user playlists.
+        """
         key: str = 'user_playlists'
         if key not in self._cache or self._cache[key] is None:
             self._cache[key] = self._spy_client.current_user_playlists()['items']
@@ -78,6 +86,9 @@ class SwaRunner:
         return self._cache[key]
 
     def get_playlist_by_name(self, name: str, multiple: bool = False) -> List[Dict]:
+        """
+        Gets a user playlist by it's name.
+        """
         matches = []
         for playlist in self.get_user_playlists():
             if playlist['name'] == name:
@@ -91,6 +102,9 @@ class SwaRunner:
         return matches
 
     def get_discover_weekly(self, allow_multiple: bool = False) -> list or dict:
+        """
+        Attempts to find the "Discover weekly" playlist.
+        """
         if self._discover_weekly_id:
             p = self._spy_client.playlist(self._discover_weekly_id)
             print('Got pre-selection: {self._discover_weekly_id}')
@@ -109,6 +123,9 @@ class SwaRunner:
         return self._cache[playlist_name] if allow_multiple else self._cache[playlist_name][0]
 
     def prepare_weekly_album_playlist(self) -> dict:
+        """
+        Attempts to find the "Weekly Album discovery", cleaning it up is needed.
+        """
         album_playlist = self.get_playlist_by_name(self._special_playlist['name'])
         if not album_playlist:
             logging.debug("Creating playlist: '%s'", self._special_playlist['name'])
@@ -133,7 +150,10 @@ class SwaRunner:
         logging.info('!')
 
     def _do_playlist_cleanup(self, playlist_id: str):
-        playlist_tracks = self._spy_client.playlist_tracks(playlist_id=playlist_id, fields='items(track(id))')
+        playlist_tracks = self._spy_client.playlist_tracks(
+            playlist_id=playlist_id,
+            fields='items(track(id))'
+        )
 
         if not playlist_tracks or len(playlist_tracks['items']) <= 0:
             return None
@@ -146,11 +166,17 @@ class SwaRunner:
         )
 
     def get_weekly_albums_ids(self):
+        """
+        Gets all the album IDs for the songs contained in the Discover Weekly playlist
+        """
         playlist = self.get_discover_weekly()
         tracks = self._spy_client.playlist_tracks(playlist_id=playlist['id'], fields='items(track(id,album(id)))')
         return [t['track']['album']['id'] for t in tracks['items']]
 
     def get_all_albums_tracks(self, album_ids: list):
+        """
+        Retrurns all the tracks for a list of album IDs
+        """
         tracks = []
         for album_id in album_ids:
             tracks.extend([t['id'] for t in self._spy_client.album_tracks(album_id)['items']])
@@ -158,6 +184,9 @@ class SwaRunner:
         return tracks
 
     def add_tracks_to_playlist(self, playlist_id: str, tracks: list):
+        """
+        Given a list of tracks and a playlists it appends them to such playloist.
+        """
         for chunk in list(SwaRunner.divide_chunks(tracks, 100)):
             self._spy_client.user_playlist_add_tracks(
                 user=self.get_username(),
@@ -167,12 +196,11 @@ class SwaRunner:
 
     @staticmethod
     def divide_chunks(items: list, size: int):
+        """
+        Generator to split a list in chunks of a given size.
+        """
         for i in range(0, len(items), size):
             yield items[i:i + size]
-
-    @staticmethod
-    def get_oauth_token(username: str, scope: str = 'user-read-email playlist-read-private playlist-modify-private'):
-        return prompt_for_user_token(username, scope)
 
     @staticmethod
     def _sort_playlists_by_author(playlists: dict) -> dict:
@@ -186,19 +214,3 @@ class SwaRunner:
             sorted_playlists[author_name].append(playlist)
 
         return sorted_playlists
-
-# def main(user: str):
-#     token = SwaRunner.get_oauth_token(user)
-#     if not token:
-#         raise RuntimeError('Unable to get oauth token.')
-#
-#     client = Spotify(auth=token)
-#     SwaRunner(client).run()
-#
-#
-# if __name__ == '__main__':
-#     if len(sys.argv) > 1:
-#         main(sys.argv[1])
-#     else:
-#         print("Usage: %s username" % (sys.argv[0],))
-#         sys.exit(1)
